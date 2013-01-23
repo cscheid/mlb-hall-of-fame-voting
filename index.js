49d9dde4f40f2ee12ef58ef59f3f21394867d215
Carlos Scheidegger <cscheid@research.att.com>
2013-01-23 15:03:46 -0500

    google analytics

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+//////////////////////////////////////////////////////////////////////////////
+// google analytics
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-19821647-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+//////////////////////////////////////////////////////////////////////////////
+
 var width = window.innerWidth * 0.70, height = window.innerHeight * 0.5;
 var margin = { top: 10, right: 20, bottom: 40, left: 50 };
 var first_year = 1936, last_year = 2013;
@@ -44,6 +58,8 @@ d3.selection.prototype.moveToFront = function() {
         return this; // for chaining...
     };
 })(jQuery);
+
+//////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////
 // which stats to search, their names, etc.
